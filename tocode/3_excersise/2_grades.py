01e1c3e6b9c582b7372d35a113dad47f9b1360ae
johnmogi <anguru@gmail.com>
2022-01-31 16:24:36 +0700

    auth_success

---
+++ tocode/3_excersise/2_grades.py
@@ -0,0 +1,24 @@
+'''
+ask user 20 times for grade
+calculate avg.
+print all grades above avg.
+'''
+grades = []
+num = 20
+result = 0
+for _ in range(num):
+    grade = int(input(f'give me a grade {_}/{num}: '))
+    grades.append(grade)
+
+for g in range(num):
+    print(grades[g])
+    result += grades[g]
+
+avg = result / num
+good_grades = []
+
+for grade in grades:
+    if grade > avg:
+        good_grades.append(grade)
+
+print(f'your good grades are: {good_grades}')
---
+++ tocode/3_excersise/heb_auth/שומשום.py
@@ -0,0 +1,27 @@
+welcome = 'שומשום נפתח עבורך אדוני'
+alert = 'השודדים באים, נוס על חייך פולש!'
+tries = 0
+with open('סיסמאות.txt', 'r', encoding='UTF-8') as f:
+    file_content = f.readlines()
+    username = input(f"מי מעז להטריד את שלוותי? \n")
+    # this happens now 4 times, hence the loop:
+    for line in file_content:
+        tries = tries + 1
+        if tries > 3:
+            print(alert)
+            break
+        temp = line.split()
+        user_name = temp[3]
+        # print(f'user_name: {user_name}')
+        if username == user_name:
+            print(f'שלום ,{username} ')
+            pass_word = temp[1].split(',')
+            pass_word = pass_word[0]
+            # print(f'pass_word: {pass_word}')
+            password = input('מה הסיסמה הסודית ביותר ביקום כולו: ')
+            if password == pass_word:
+                print(welcome)
+                break
+            else:
+                print(alert)
+
